27d3f060f10f38c661053ea19431021f8c6709fd
eangmengly73@gmail.com <eangmengly73@gmail.com>
2026-03-15 23:53:14 +0700

    Update EX5

--- Material/Week_1/LAB01/L01EX5.java
+++ Material/Week_1/LAB01/L01EX5.java
@@ -20,5 +20,36 @@ public class L01EX5 {
         System.out.println("'i' is at index: " + textC.indexOf('i'));
         System.out.println("'u' is at index: " + textC.indexOf('u'));
         System.out.println("'e' is at index: " + textC.indexOf('e'));
+
+        //Part D
+        System.out.println("\n\n" + "=".repeat(15) + "  Part D  " + "=".repeat(15));
+        String textD1 = "Hi Students!";
+        String textD2 = "Teacher";
+        String textD3 = "Student";
+        if (textD1.equals(textD2)) {
+            System.out.println("Text1 is equal to Text2: " + true);
+        } else {
+            System.out.println("Text1 is equal to Text2: " + false);
+        }
+
+        if (textD1.equals(textD3)) {
+            System.out.println("Text1 is equal to Text3: " + true);
+        } else {
+            System.out.println("Text1 is equal to Text3: " + false);
+        }
+
+        //Part E
+        System.out.println("\n\n" + "=".repeat(15) + "  Part E  " + "=".repeat(15));
+        if (textD1.contains(textD2)) {
+            System.out.println("Text1 contains Text2: " + true);
+        } else {
+            System.out.println("Text1 contains Text2: " + false);
+        }
+
+        if (textD1.contains(textD3)) {
+            System.out.println("Text1 contains Text3: " + true);
+        } else {
+            System.out.println("Text1 contains Text3: " + false);
+        }
     }
 }
